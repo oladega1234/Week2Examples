c94020e5db421eedf123cee3e9a4e5ba92d1156f
oladega1234 <oladega@sheridancollege.ca>
2023-01-16 11:47:48 -0500

    student project version 1

---
+++ src/printstudentlist2/PrintStudentList2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package printstudentlist2;
+
+/**
+ *
+ * @author Fuwad Oladega
+ */
+public class PrintStudentList2 {
+
+    
+    public static void main(String[] args) {
+     student[] list= new student[3];
+     student s1=new  student("Fuwad",20);
+     student s2=new  student("Kosta",9);
+     student s3=new  student("Fawas",21); 
+     list[0]=s1;
+     list[1]=s2;
+     list[2]=s3;
+     for(int i=0; i<list.length; i++)
+     {
+         System.out.println(list[i].getName() + " " +list[i].getAge());
+     }
+
+
+     
+    }
+    
+}
---
+++ src/printstudentlist2/student.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package printstudentlist2;
+
+/**
+ *
+ * @author Fuwad Oladega
+ */
+public class student {
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+     private   String name;
+     private int age;
+        public student(String name, int age)
+        {
+            this.name=name;
+            this.age=age;
+        }
+    
+}
